35774b8ec9d40e7e0046bf5276f61cf0b3105369
henrik242 <henrik242@users.noreply.github.com>
2024-01-11 16:20:16 +0100

    procenv 1.3 (new formula)

--- Formula/p/procenv.rb
+++ Formula/p/procenv.rb
@@ -1,8 +1,8 @@
 class Procenv < Formula
   desc "Show process environment variables"
   homepage "https://github.com/henrik242/procenv"
-  url "https://github.com/henrik242/procenv/archive/refs/tags/1.1.tar.gz"
-  sha256 "7371164f700c695fe27783b4471c2817e229fa7acc20d7a8b37a49687ef5f65d"
+  url "https://github.com/henrik242/procenv/archive/refs/tags/1.3.tar.gz"
+  sha256 "34a0be871d5a18a0ccce9747b3c62da37d8b771f74df4feab7bc40fe933b91d9"
   license "MIT"
 
   def install
@@ -11,6 +11,7 @@ class Procenv < Formula
   end
 
   test do
-    system "#{bin}/procenv"
+    assert_equal "Unaccessible or missing PID: 1234567890", shell_output("#{bin}/procenv 1234567890").strip
+    assert_equal "Illegal PID: FOO", shell_output("#{bin}/procenv FOO").strip
   end
 end
